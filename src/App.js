20def645c41a99e520081660bde634f7a9fa3422
caleb <calebakpan7@gmail.com>
2022-04-23 14:03:30 -0700

    added onClick handler to start quiz btn

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ export default function App () {
 
     const [Questions, setQuestions] = React.useState([])
 
+    function requestData () {
+        console.log("function ran")
+    }
+
+
     return (
         <div>
-            <img className="blob-5" src={'./images/blob 5.png'}></img>            
-            {Questions.length < 1 ? <StartPage /> : <QuestionPage />}
+            <img className="blob-5" src={'./images/blob 5.png'}></img>
+            {Questions.length < 1 ? <StartPage Data={requestData}/> : <QuestionPage />}
             <img className="blob-4" src={'./images/blob 4.png'}></img>
         </div>
     )
--- src/components/startPage.js
+++ src/components/startPage.js
@@ -1,11 +1,12 @@
 
-export default function start() {
+export default function start(props) {
+
     return (
         <div className="startPage">
             <div className="startPage-el">
                 <h1>Quizzical</h1>
                 <p>A fun quiz game</p>
-                <button>Start quiz</button>
+                <button onClick={props.Data}>Start quiz</button>
             </div>
         </div>
         
